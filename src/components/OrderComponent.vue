f6dcda32ba5d19c13dcc10b47afaf5362fc3353f
CP96 <cristian.pop6@yahoo.com>
2021-06-18 20:54:23 +0300

    Order View

---
+++ src/components/OrderComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div v-if="items">
+    <h3>{{ title }}</h3>
+    <div v-for="(item, index) in items" :key="index">
+      <p>
+        Model: {{ item.model }} , {{ size(item) }} , Count: {{ item.count }} ,
+        Price: {{ item.price }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OrderComponent",
+  props: {
+    items: Array,
+    title: String,
+  },
+  methods: {
+    size(item) {
+      if (item.size) {
+        return "Size: " + item.size;
+      } else if (item.lenght) {
+        return "Length: " + item.lenght;
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/admin-page/OrdersView.vue
+++ src/views/admin-page/OrdersView.vue
@@ -1,20 +1,42 @@
 <template>
   <section>
     <h2>Orders</h2>
-     <table class="pure-table pure-table-horizontal">
+    <table class="pure-table pure-table-horizontal">
       <thead>
         <tr>
           <th scope="col">#</th>
           <th scope="col">
             User
           </th>
+          <th scope="col">Order Date</th>
           <th scope="col">Actions</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(item, index) in orders" :key="item.id">
-          <td>{{ index + 1 }}</td>
-        </tr>
+        <template v-for="(item, index) in orders">
+          <tr :key="index + '1'">
+            <td>{{ index + 1 }}</td>
+            <td>{{ item.id }}</td>
+            <td>{{ item.orderDate.toDate().toDateString() }}</td>
+            <td>
+              <button
+                type="button"
+                class="pure-button button-secondary"
+                v-on:click="displayDetails(item)"
+              >
+                View Order
+              </button>
+            </td>
+          </tr>
+          <tr :key="index + '2'" v-if="item.showDetails">
+            <td colspan="5">
+              <OrderComponent :items="item.cartSkis" title="Skis" />
+              <OrderComponent :items="item.cartSkiBoots" title="Ski Boots" />
+              <OrderComponent :items="item.cartSnowboards" title="Snowboards" />
+              <OrderComponent :items="item.cartBoots" title="Boots" />
+            </td>
+          </tr>
+        </template>
       </tbody>
     </table>
   </section>
@@ -22,11 +44,14 @@
 
 <script>
 import db from "@/data-provider";
+import OrderComponent from "@/components/OrderComponent.vue";
+
 export default {
+  components: { OrderComponent },
   name: "OrdersView",
   data() {
     return {
-        orders:[],
+      orders: [],
     };
   },
   mounted() {
@@ -36,16 +61,45 @@ export default {
         this.orders = querySnapshot.docs.map((doc) => {
           const data = doc.data();
 
-          return { id: doc.id, ...data };
+          return {
+            id: doc.id,
+            showDetails: false,
+            cartSkis: [],
+            cartSkiBoots: [],
+            cartSnowboards: [],
+            cartBoots: [],
+            ...data,
+            loading: true,
+          };
         });
-        console.log(this.orders)
-      })
-    //   .then(() => {
-    //     console.log("Order Saved");
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-      //});
+        
+      });
+  },
+  methods: {
+    queryData(queryRef, container) {
+      if (!container || (container && !container.lenght)) {
+        db.collection(queryRef).onSnapshot((querySnapshot) => {
+          querySnapshot.docs.map((doc, index) => {
+            const data = doc.data();
+
+            this.$set(container, index, data);
+          });
+        });
+      }
+    },
+    displayDetails(item) {
+      item.showDetails = !item.showDetails;
+      const skiRef = item.skis;
+      const skiBootsRef = item.skiBoots;
+      const snowboardsRef = item.snowboards;
+      const bootsRef = item.boots;
+      this.queryData(skiRef, item.cartSkis);
+      this.queryData(skiBootsRef, item.cartSkiBoots);
+      this.queryData(snowboardsRef, item.cartSnowboards);
+      this.queryData(bootsRef, item.cartBoots);
+
+      
+    },
   },
 };
 </script>
